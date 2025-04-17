0bd6533ab34ac391df7b4c12837c6752f13bccce
Igbinosa Osasuyi David <igbinosasasu@gmail.com>
2025-04-17 22:07:43 +0100

    added google login and creating google meet  live meeting

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3002/auth/google/callback", // Match exactly what's in Google Console
+      callbackURL: `${process.env.SERVER_URL}auth/google/callback`, // Match exactly what's in Google Console
       passReqToCallback: true,
       scope: [
         "profile",
